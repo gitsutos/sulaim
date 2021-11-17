78df546ba0949fd5d1df7978a3ed6047b1663c95
Shifan sulaim <gitsutos@yahoo.com>
2021-11-17 10:17:59 +0530

    make it better

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mysite',
+    'todoapp',
 ]
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
