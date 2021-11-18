2216327dd09df735ec3ae8c857cbf4eecf7fccc6
Shifan sulaim <gitsutos@yahoo.com>
2021-11-18 19:22:58 +0530

    version 0.03

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+User = settings.AUTH_USER_MODEL
+
 class Todo(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   added_date = models.DateTimeField()
   text = models.CharField(max_length = 200)
   amount = models.FloatField()
