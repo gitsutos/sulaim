eb50f606bbe89bd4fbd205bd9706adf55f92d9f2
Shifan sulaim <gitsutos@yahoo.com>
2021-11-22 14:27:44 +0530

    V 1.3

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,11 +23,11 @@ from todoapp.views import user_login,user_logout,use_sign_up
 urlpatterns = [
     path('admin/', admin.site.urls),    
     path('cost-manager-by-tos/',include('todoapp.urls')),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
+    path('favicon.ico', RedirectView.as_view('static/img/favicon.ico')),
     path('',RedirectView.as_view(url='cost-manager-by-tos/')),
     path('login/', user_login),
     path('log_out/', user_logout),
-    path('sign_up/', use_sign_up),
+    path('/', use_sign_up),
 
 ]
 
