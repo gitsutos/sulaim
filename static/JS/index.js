ce26b0891e0e35bffaf0f36435e2a423568e815b
Shifan sulaim <gitsutos@yahoo.com>
2021-11-24 13:33:23 +0530

    bug fixing

---
+++ static/JS/index.js
@@ -0,0 +1,61 @@
+
+const tableDivElement = $("#table_body_element");
+const completeCostElement = $("#complete_cost");
+
+function loadCostTable(costTableEl) {
+const xhr = new XMLHttpRequest();
+//xhr.setRequestHeader("HTTP_X_REQUESTED_WITH", "XMLHttpRequest")
+//xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest")
+const method = "GET";
+const url = "/cost-manager-by-tos/list_view_of_costs";
+const responseType = "json";
+
+xhr.responseType = responseType;
+xhr.open(method, url);
+xhr.onload = function () {
+    const serverResponse = xhr.response;
+    var listStrTemp = "";
+    var listedItems = serverResponse.response;
+    var i;
+    var TempAmount = 0;
+
+    for (i = listedItems.length - 1; i > -1; i--) {
+    var element = listedItems[i];
+    //console.log(element)
+    TempAmount += element.amount;
+    var currentItem = formatedCostElements(element);
+    listStrTemp += currentItem;
+    }
+    costTableEl.replaceWith(listStrTemp);
+    completeCostElement.replaceWith(completeCostElement);
+};
+xhr.send();
+completeCostElement.replaceWith(completeCostElement);
+}
+
+loadCostTable(tableDivElement);
+
+function deleteButton(obj) {
+return (
+    "<form action='/cost-manager-by-tos/delete_todo/" +
+    obj.id +
+    "' method='post'>" +
+    "<div class='float-right'>" +
+    "<button type='submit' class='btn btn-danger'>Delete</button>" +
+    "</div>" +
+    "</form>"
+);
+}
+function formatedCostElements(costarg) {
+var formatedcCE =
+"<div class='row'>"+
+    "<li class='col-4'>" +
+    costarg.amount +
+    "</li><li class='col-4'>" +
+    costarg.person_used +
+    "</li><li class='col-4'>" +
+    costarg.description +
+    deleteButton(costarg) +
+    "</li></div>"
+return formatedcCE;
+}
