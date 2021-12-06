2529aef31702fdf0940c581ee543cb33ec9e15df
Shifan sulaim <gitsutos@yahoo.com>
2021-12-06 13:24:23 +0530

    bug fixing

--- static-root/JS/main/home.js
+++ static-root/JS/main/home.js
@@ -11,36 +11,55 @@ function loadCostTable(costTableEl) {
   xhr.responseType = responseType;
   xhr.open(method, url);
   xhr.onload = function () {
-    const serverResponse = xhr.response;
-    var listStrTemp = "";
-    var listedItems = serverResponse.costs_list;
-    var i;
-
-    for (i = listedItems.length - 1; i > -1; i--) {
-      var element = listedItems[i];
-      var currentItem = formatedCostElements(element);
-      listStrTemp += currentItem;
+    if (xhr.status === 401) {
+      $("#main_body").replaceWith(`<div class="jumbotron">
+        <h1>Login requird</h1>
+        <h3><p>you must login to add, delete and see your costs </p></h3>
+        <p>if you already have an tos acount then <a href="/login/">login</a> in <br>
+        if you don't have click the "<a href="/sign_up">Sign_up</a>" link and create an account and then login</p>
+      </div>`)
     }
-    costTableEl.replaceWith(listStrTemp);
+    if (xhr.status === 200) {
+      const serverResponse = xhr.response;
+      const total_of_month = total_cost_of_m(serverResponse);
+      const total_of_year = total_cost_of_y(serverResponse);
+      $("#total_cost_of_m").replaceWith(total_of_month)
+      $("#total_cost_of_y").replaceWith(total_of_year)
+    };
   };
   xhr.send();
 }
 
 loadCostTable(tableDivElement);
 
-function deleteButton(obj) {
-  return (
-    "<form action='/delete_todo/" +
-    obj.id +
-    "' method='post'>" +
-    "<div class='float-right'>" +
-    "<button type='submit' class='btn btn-danger'>Delete</button>" +
-    "</div>" +
-    "</form>"
-  );
+function total_cost_of_m(listed_items) {
+  var total = 0
+  for (let i = 0; i < listed_items.length; i++) {
+    const element = listed_items[i];
+    var date = new Date(element.added_date)
+    var today = new Date()
+    if (date.getMonth() === today.getMonth()) {
+      total = total + element.amount
+    }
+  }
+  return (total);
 }
-
-function formatedCostElements(costarg) {
-  var formatedcCE = "";
-  return formatedcCE;
+function total_cost_of_y(listed_items) {
+  var total = 0
+  for (let i = 0; i < listed_items.length; i++) {
+    const element = listed_items[i];
+    var date = new Date(element.added_date)
+    var today = new Date()
+    if (date.getFullYear() === today.getFullYear()) {
+      total = total + element.amount
+    }
+  }
+  return (total);
 }
+$("#main_body").css({
+  "display": "none",
+})
+$(document).ready(
+  $("#main_body").css({
+    "display": "contents",
+  }))
